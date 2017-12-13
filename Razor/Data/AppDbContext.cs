3197d7eff355707a66b69258f23aefbd7a8893bb
Gordon-mac <devgordonlee@gmail.com>
2017-12-14 00:38:09 +0900

    Add post message case.

---
+++ Razor/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace web.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
