7447c865fe06c8d4c49352a8f2dc050d6b6e92b6
이영득 [gordonlee] <devgordonlee@gmail.com>
2017-12-14 16:13:36 +0900

    Add multiple Db connection with single pool

---
+++ multiple_dbcontext_web/Controllers/ValuesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace multiple_dbcontext_web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private DbContextPool _pool { get; set; } = null;
+
+        public ValuesController(DbContextPool pool)
+        {
+            _pool = pool;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (_pool.PoolDict.ContainsKey(id))
+            {
+                DbContext context = null;
+                if (_pool.PoolDict.TryGetValue(id, out context))
+                {
+                    if (context is CustomDbContext)
+                    {
+                        int contextId = (context as CustomDbContext).ID;
+                        return "Context ID is " + contextId.ToString();
+                    }
+                    else
+                    {
+                        return "This is not CustomDbContext";
+                    }
+                }
+                else
+                {
+                    return "Failed to get value of dictionary.";
+                }
+            }
+            else
+            {
+                return "Cannot find serviceId.";
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ multiple_dbcontext_web/DbContextPool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace multiple_dbcontext_web
+{
+    public class CustomDbContext : DbContext
+    {
+        public int ID { get; set; } = -1;
+        public CustomDbContext(DbContextOptions options, int id ) 
+            : base(options)
+        {
+            ID = id;
+        }
+    }
+
+
+    //MEMO: singleton call.
+    public class DbContextPool
+    {
+        // int(key) : serviceID, DbContext(value) : class obj
+        public Dictionary<int, DbContext> PoolDict { get; set; } = new Dictionary<int, DbContext>();
+
+
+        public DbContextPool(IServiceCollection services)
+        {
+            var builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase("TEST2");
+            PoolDict.Add(2, new CustomDbContext(builder.Options, 2));
+
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase("TEST3");
+            PoolDict.Add(3, new CustomDbContext(builder.Options, 3 ));
+        }
+
+        private DbContext CreateContext(int serviceId)
+        {
+            var builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(string.Format("{0}", serviceId));
+            return new CustomDbContext(builder.Options, serviceId);
+        }
+    }
+}
